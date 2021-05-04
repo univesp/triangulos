97c6f328516707014f89ec87f0ae283bc52329b4
AlbertoOrtizMendes <alberto.ortiz.mendes@gmail.com>
2021-05-04 11:01:07 -0300

    Modelo inicial

---
+++ javascripts/events.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+  //////////////////////////////////////////////////////////////////
+  // HEADER DINÂMICO
+  // Mostra header somente no início da página.
+  // Descomentar caso utilizada a classe .header-dinamico. Caso contrário, deletar.
+
+    $(window).scroll(function(){
+      var nav = $(".header-dinamico .container");
+      var scroll = $(window).scrollTop();
+      if(scroll == 0){
+        nav.fadeIn();
+      } else {
+        nav.fadeOut();
+      }
+    });
+
+  //////////////////////////////////////////////////////////////////
+
+  // Seu código abaixo
+
+})
